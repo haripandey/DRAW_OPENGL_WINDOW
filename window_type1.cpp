f99f01d380c2473c0927e8e2dbebb66971e56dff
Hari <haripandey@users.noreply.github.com>
2015-03-29 19:00:17 -0700

    Create window_type1.cpp

---
+++ window_type1.cpp
@@ -0,0 +1,16 @@
+#include <GL/glfw.h>
+int main(int argc, char** argv)
+{
+    glfwInit();
+    glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
+    glfwOpenWindow(480*8/9, 480,8,8,8,8,0,0,  GLFW_WINDOW);
+    glfwSetWindowTitle("HARI's  FIRST C++ OPENGL SNAKE GAME_WINDOW");
+    bool running=true;
+
+    while (running){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers();
+   running =glfwGetWindowParam(GLFW_OPENED);
+}
+   glfwTerminate();
+}
